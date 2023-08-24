1f24071216eb7ba737cbccf892707ac02ee24f56
Gabriel Pires <gabpiresdev@gmail.com>
2023-08-24 18:24:17 -0300

    feat: sign in screen with new input component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,5 @@
-import { Center, Image, Text, VStack } from 'native-base'
+import { Input } from '@components/Input'
+import { Center, Heading, Image, Text, VStack } from 'native-base'
 
 import backgroundImage from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
@@ -7,7 +8,7 @@ interface SignInProps {}
 
 export function SignIn(props: SignInProps) {
   return (
-    <VStack flex={1} bg={'gray.700'}>
+    <VStack flex={1} bg={'gray.700'} px={10}>
       <Image
         source={backgroundImage}
         alt="Imagem com pessoas treinando"
@@ -18,8 +19,29 @@ export function SignIn(props: SignInProps) {
       <Center my={24}>
         <LogoSvg />
         <Text color={'gray.100'} fontSize={'sm'}>
-          Treine sua mente e o corpo
+          Treine sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading
+          color={'gray.100'}
+          fontSize={'xl'}
+          mb={6}
+          fontFamily={'heading'}
+        >
+          Acesse sua conta
+        </Heading>
+
+        <VStack flex={1} w={'100%'} space={4}>
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <Input placeholder="Senha" secureTextEntry />
+        </VStack>
       </Center>
     </VStack>
   )
