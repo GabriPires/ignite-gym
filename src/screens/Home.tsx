b978a765a388c04571aa4b35497bfcd8fa8595dc
Gabriel Pires <gabpiresdev@gmail.com>
2023-09-14 13:39:11 -0300

    feat: new exercise card component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
 import { FlatList, HStack, Heading, Text, VStack } from 'native-base'
@@ -47,6 +48,9 @@ export function Home() {
             4
           </Text>
         </HStack>
+
+        <ExerciseCard />
+        <ExerciseCard />
       </VStack>
     </VStack>
   )
