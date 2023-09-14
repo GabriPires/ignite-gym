8f65e102dd29dcda893187515cbd7d1250ae4669
Gabriel Pires <gabpiresdev@gmail.com>
2023-09-14 13:27:05 -0300

    feat: flatlist to list groups

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -2,9 +2,10 @@ import { IPressableProps, Pressable, Text } from 'native-base'
 
 interface GroupProps extends IPressableProps {
   name: string
+  isActive: boolean
 }
 
-export function Group({ name, ...props }: GroupProps) {
+export function Group({ name, isActive, ...props }: GroupProps) {
   return (
     <Pressable
       mr={3}
@@ -15,10 +16,15 @@ export function Group({ name, ...props }: GroupProps) {
       justifyContent="center"
       alignItems="center"
       overflow="hidden"
+      isPressed={isActive}
+      _pressed={{
+        borderColor: 'green.500',
+        borderWidth: 1,
+      }}
       {...props}
     >
       <Text
-        color="gray.200"
+        color={isActive ? 'green.500' : 'gray.200'}
         textTransform="uppercase"
         fontSize="xs"
         fontWeight="bold"
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,41 @@
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { VStack } from 'native-base'
+import { FlatList, VStack } from 'native-base'
+import { useState } from 'react'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas')
+  const [groups, setGroups] = useState([
+    'costas',
+    'ombro',
+    'peito',
+    'biceps',
+    'triceps',
+    'perna',
+  ])
+
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Group name="costas" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+      />
     </VStack>
   )
 }
