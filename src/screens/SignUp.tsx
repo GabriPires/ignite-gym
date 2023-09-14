ac56eb9c40b4f894526578b50003898416c25256
Gabriel Pires <gabpiresdev@gmail.com>
2023-09-14 09:09:36 -0300

    feat: sign up screen

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,60 @@
+import { Input } from '@components/Input'
+import { Center, Heading, Image, ScrollView, Text, VStack } from 'native-base'
+
+import backgroundImage from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg'
+import { Button } from '@components/Button'
+
+interface SignUpProps {}
+
+export function SignUp(props: SignUpProps) {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+      bg={'gray.700'}
+    >
+      <VStack flex={1} px={10} pb={16}>
+        <Image
+          source={backgroundImage}
+          alt="Imagem com pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
+
+        <Center my={24}>
+          <LogoSvg />
+          <Text color={'gray.100'} fontSize={'sm'}>
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading
+            color={'gray.100'}
+            fontSize={'xl'}
+            mb={6}
+            fontFamily={'heading'}
+          >
+            Crie sua conta
+          </Heading>
+
+          <Input placeholder="Nome" mb={4} />
+
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            mb={4}
+          />
+
+          <Input placeholder="Senha" secureTextEntry mb={4} />
+
+          <Button title="Criar e acessar" />
+        </Center>
+
+        <Button title="Voltar para o login" variant={'outline'} mt={24} />
+      </VStack>
+    </ScrollView>
+  )
+}
