2963a25f75db7027da388850a7adefad250de00a
Gabriel Pires <gabpiresdev@gmail.com>
2023-09-14 13:06:19 -0300

    feat: finish home header component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,4 +1,6 @@
-import { HStack, Heading, Text, VStack } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
+import { HStack, Heading, Icon, Text, VStack } from 'native-base'
+import { TouchableOpacity } from 'react-native'
 import { UserPhoto } from './UserPhoto'
 
 export function HomeHeader() {
@@ -10,7 +12,7 @@ export function HomeHeader() {
         size={16}
         mr={4}
       />
-      <VStack>
+      <VStack flex={1}>
         <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
@@ -18,6 +20,10 @@ export function HomeHeader() {
           Gabriel
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
     </HStack>
   )
 }
