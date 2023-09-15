6e4ec087540b804231785664954f26c97d819873
Gabriel Pires <gabpiresdev@gmail.com>
2023-09-15 08:14:13 -0300

    feat: user photo and loading state with skeleton on profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,36 @@
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
+import { UserPhoto } from '@components/UserPhoto'
+import { Center, ScrollView, Skeleton, VStack } from 'native-base'
+import { useState } from 'react'
+
+const photoSize = 33
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={photoSize}
+              h={photoSize}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/GabriPires.png' }}
+              alt="Foto do usuário"
+              size={photoSize}
+            />
+          )}
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
