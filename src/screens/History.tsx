f31f15794373caf2cbcd4fd301f90f75d5491dc3
Gabriel Pires <gabpiresdev@gmail.com>
2023-09-15 07:58:42 -0300

    feat: new section list to history screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,14 +1,40 @@
 import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
+import { Heading, SectionList, VStack } from 'native-base'
+import { useState } from 'react'
+
+type Exercise = {
+  title: string
+  data: string[]
+}
 
 export function History() {
+  const [exercises, setExercises] = useState<Exercise[]>([
+    {
+      data: ['Puxada frontal', 'Remada curvada'],
+      title: '26.08.2023',
+    },
+    {
+      data: ['Puxada frontal', 'Remada curvada'],
+      title: '27.08.2023',
+    },
+  ])
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de exercícios" />
 
-      <HistoryCard />
-      <HistoryCard />
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        px={8}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {title}
+          </Heading>
+        )}
+      />
     </VStack>
   )
 }
