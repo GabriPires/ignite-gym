74846154efa0112ebae7f451f42c126e1b7da49e
Gabriel Pires <gabpiresdev@gmail.com>
2023-09-20 18:36:17 -0300

    feat: use hook form to handle with sign up form

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -2,11 +2,41 @@ import backgroundImage from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useNavigation } from '@react-navigation/native'
 import { Center, Heading, Image, ScrollView, Text, VStack } from 'native-base'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const signUpSchema = z
+  .object({
+    name: z.string().min(3, 'Nome deve ter no mínimo 3 caracteres'),
+    email: z.string().email('E-mail inválido'),
+    password: z.string().min(6, 'Senha deve ter no mínimo 6 caracteres'),
+    confirmPassword: z.string().min(6, 'Senha deve ter no mínimo 6 caracteres'),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password !== data.confirmPassword) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Senhas não conferem',
+        path: ['confirmPassword'],
+      })
+    }
+  })
+
+type SignUpSchemaType = z.infer<typeof signUpSchema>
 
 export function SignUp() {
   const navigation = useNavigation()
+
+  const { control, handleSubmit } = useForm<SignUpSchemaType>({
+    resolver: zodResolver(signUpSchema),
+  })
+
+  function handleSignUp(data: SignUpSchemaType) {
+    console.log(data)
+  }
 
   return (
     <ScrollView
@@ -39,18 +69,66 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input placeholder="Nome" mb={4} />
-
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
-            mb={4}
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Nome"
+                mb={4}
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
           />
 
-          <Input placeholder="Senha" secureTextEntry mb={4} />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                mb={4}
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
+          />
 
-          <Button title="Criar e acessar" />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                mb={4}
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="confirmPassword"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Confirme a senha"
+                secureTextEntry
+                mb={4}
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
+          />
+
+          <Button
+            title="Criar e acessar"
+            onPress={handleSubmit(handleSignUp)}
+          />
         </Center>
 
         <Button
