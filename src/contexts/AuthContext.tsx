b004979d1a49642d5576ab76d8cba15f8c7b5034
Gabriel Pires <gabpiresdev@gmail.com>
2023-09-22 12:19:08 -0300

    refactor: state to user on auth context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,22 +1,30 @@
 import { UserDTO } from '@dtos/UserDTO'
-import { ReactNode, createContext, useContext } from 'react'
+import { ReactNode, createContext, useContext, useState } from 'react'
 
 interface AuthContextProps {
   user: UserDTO
+  setUser: (user: UserDTO) => void
 }
 
 const AuthContext = createContext<AuthContextProps>({} as AuthContextProps)
 
-export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+interface AuthContextProviderProps {
+  children: ReactNode
+}
+
+export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+  const [user, setUser] = useState<UserDTO>({
+    id: '1',
+    name: 'Gabriel Pires',
+    email: 'gabriel@email.com',
+    avatar: 'https://github.com/GabriPires.png',
+  })
+
   return (
     <AuthContext.Provider
       value={{
-        user: {
-          id: '1',
-          name: 'Gabriel Pires',
-          email: 'gabriel@email.com',
-          avatar: 'https://github.com/GabriPires.png',
-        },
+        user,
+        setUser,
       }}
     >
       {children}
@@ -24,7 +32,7 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   )
 }
 
-export const useAuthContext = () => {
+export const useAuth = () => {
   const context = useContext(AuthContext)
   if (context === undefined) {
     throw new Error('useAuthContext must be used within a AuthContextProvider')
