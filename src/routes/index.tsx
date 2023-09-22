161d174326506048784e644d424633877fb8852b
Gabriel Pires <gabpiresdev@gmail.com>
2023-09-22 12:44:02 -0300

    feat: redirect user when is authenticated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,12 @@
+import { useAuth } from '@contexts/AuthContext'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { Box, useTheme } from 'native-base'
+import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
   const { colors } = useTheme()
+  const { user } = useAuth()
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
@@ -11,7 +14,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
