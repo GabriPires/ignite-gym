ba543a0720ff795ea785f01ea11d28d3ab93b672
Gabriel Pires <gabpiresdev@gmail.com>
2023-09-27 14:29:09 -0300

    feat: show user data on profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,13 +2,24 @@ import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
+import { useAuth } from '@contexts/AuthContext'
+import { zodResolver } from '@hookform/resolvers/zod'
 import * as FileSystem from 'expo-file-system'
 import * as ImagePicker from 'expo-image-picker'
 import { Center, Heading, Skeleton, Text, VStack, useToast } from 'native-base'
 import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 import { ScrollView, TouchableOpacity } from 'react-native'
+import { z } from 'zod'
 
 const photoSize = 33
+
+const formDataSchema = z.object({
+  name: z.string().nonempty({ message: 'O nome não pode ser vazio' }),
+  email: z.string().email({ message: 'E-mail inválido' }),
+})
+
+type FormData = z.infer<typeof formDataSchema>
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
@@ -16,7 +27,21 @@ export function Profile() {
     'https://github.com/GabriPires.png',
   )
 
+  const { user } = useAuth()
+
   const { show } = useToast()
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(formDataSchema),
+    defaultValues: {
+      name: user.name,
+      email: user.email,
+    },
+  })
 
   async function handleSelectUserPhoto() {
     setPhotoIsLoading(true)
@@ -88,8 +113,32 @@ export function Profile() {
             </Text>
           </TouchableOpacity>
 
-          <Input placeholder="Nome" bg="gray.600" mb={4} />
-          <Input placeholder="E-mail" bg="gray.600" isDisabled />
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Nome"
+                bg="gray.600"
+                mb={4}
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { value } }) => (
+              <Input
+                placeholder="E-mail"
+                bg="gray.600"
+                isDisabled
+                value={value}
+              />
+            )}
+          />
 
           <Heading
             fontFamily={'heading'}
